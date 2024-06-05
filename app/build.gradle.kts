5166828db2b9b2de16ed5a018b36a0ce6bdd1416
AgustinBartaburu <agustinxagustin@gmail.com>
2024-06-05 19:10:30 -0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.acreditads2023"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.acreditads2023"
+        minSdk = 27
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    packagingOptions {
+        exclude ("META-INF/DEPENDENCIES")
+        exclude ("META-INF/LICENSE")
+        exclude ("META-INF/LICENSE.txt")
+        exclude ("META-INF/license.txt")
+        exclude ("META-INF/NOTICE")
+        exclude ("META-INF/NOTICE.txt")
+        exclude ("META-INF/notice.txt")
+        exclude ("META-INF/ASL2.0")
+        exclude ("META-INF/INDEX.LIST")
+    }
+}
+
+
+dependencies {
+
+
+    implementation ("com.android.volley:volley:1.2.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-location:21.2.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.apis:google-api-services-sheets:v4-rev20240402-2.0.0")
+    implementation ("com.google.api-client:google-api-client:2.5.1")
+    implementation ("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+
+}
---
+++ app/src/main/java/com/example/acreditads2023/Evento_activity.java
@@ -0,0 +1,125 @@
+package com.example.acreditads2023;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class Evento_activity extends AppCompatActivity {
+    private EditText edtTituloEvento;
+    private EditText edtAutorEvento;
+    private EditText edtTipoEvento;
+    private EditText edtFechaHoraEvento;
+    private EditText edtIdAdminEvento;
+    private EditText edtQRaperturaEvento;
+    private EditText edtQRcierreEvento;
+    private Button btnAgregar;
+    private Button btnVolver;
+    private Database db;
+    Button btnMapa;
+    private RequestQueue requestQueue;
+
+    private static final int REQUEST_CODE_MAP = 1;
+    String Latitud;
+    String Longitud;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.evento_activity);
+        requestQueue = Volley.newRequestQueue(this);
+        btnAgregar = findViewById(R.id.btnAgregar);
+        btnVolver = findViewById(R.id.btnVolver);
+        edtTituloEvento = findViewById(R.id.edtTipoEvento);
+        edtAutorEvento = findViewById(R.id.edtAutorEvento);
+        edtTipoEvento = findViewById(R.id.edtTipoEvento);
+        edtFechaHoraEvento = findViewById(R.id.edtFechaHoraEvento);
+        edtIdAdminEvento = findViewById(R.id.edtIdAdminEvento);
+        edtQRaperturaEvento = findViewById(R.id.edtQRaperturaEvento);
+        edtQRcierreEvento = findViewById(R.id.edtQRcierreEvento);
+
+        db = new Database(getApplicationContext());
+
+        btnAgregar = findViewById(R.id.btnAgregar);
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Evento e = new Evento();
+                e.setTituloEvento(edtTituloEvento.getText().toString());
+                e.setAutorEvento(edtAutorEvento.getText().toString());
+                e.setTipoEvento(Integer.parseInt(edtTipoEvento.getText().toString()));
+                e.setFechaHoraEvento(edtFechaHoraEvento.getText().toString());
+                e.setIdAdminEvento(Integer.parseInt(edtIdAdminEvento.getText().toString()));
+
+                EventoDAO eDAO = new EventoDAO(db);
+                eDAO.salvar(e);
+
+                // Google Sheets URL
+                //Eu preciso puxar as variáveis aqui
+                String urlSheets = "https://script.google.com/macros/s/AKfycbxX1EInivW85EH1IGNZzh6bWJTLm6K9-ZRr3-wduHYFd34d1SW0dG2bAndm8CZvYPE/exec?action=evento&tituloEvento=" +
+                        edtTituloEvento.getText().toString() + "&autorEvento=" + edtAutorEvento.getText().toString() +
+                        "&tipoEvento=" + edtTipoEvento.getText().toString() + "&fechaHoraEvento=" + edtFechaHoraEvento.getText().toString() + "&latitud=" + Latitud + "&longitud=" + Longitud;
+
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, urlSheets, new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+                        Log.i("Response", s);
+                    }
+                }, new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(com.android.volley.VolleyError error) {
+                        Log.e("Error", error.toString());
+                    }
+                });
+
+                requestQueue.add(stringRequest);
+            }
+        });
+
+        btnVolver = findViewById(R.id.btnVolver);
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMapa = findViewById(R.id.btnAgregarMapa);
+        btnMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombreEvento = edtTituloEvento.getText().toString();
+                Intent intent = new Intent(Evento_activity.this, UbicarEventoActivity.class);
+                intent.putExtra("NombreEvento",nombreEvento);
+                startActivityForResult(intent, REQUEST_CODE_MAP);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,@Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_MAP && resultCode == RESULT_OK) {
+            if (data != null) {
+                // Obtener los valores de las variables devueltas
+                Latitud = data.getStringExtra("Latitud");
+                Longitud = data.getStringExtra("Longitud");
+                Log.i("Latitud", Latitud);
+                Log.i("Longitud", Longitud);
+            }
+        }
+    }
+}
